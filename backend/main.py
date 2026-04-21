fdf7e329a2b0bd086b19873394466d785696d1ba
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2026-04-21 10:58:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 from hashtag_engine import generate_hashtags
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class Request(BaseModel):
     text: str
 
 @app.post("/generate")
 def generate(req: Request):
-    tags = generate_hashtags(req.text)
-    return {"hashtags": tags}
+    return {
+        "input": req.text,
+        "hashtags": generate_hashtags(req.text)
+    }
